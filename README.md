5bafff8c7b95a67f73a3eb83bdd7e9a5dd2ee092
alwalvicDevSystem <alwalvicdev@gmail.com>
2025-11-21 21:00:57 -0300

    01.Manipulação de String - atividades dos vídeos 1 ao 5 realizado

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Python: crie sua primeira aplicação
+
+## Executando um scrip python
+
+Para executar um script criado, dê o comando:
+```bash
+    python app.py
+```
---
+++ app.py
@@ -0,0 +1,24 @@
+#Aplicativo Sabor Express
+print("""
+╭━━━╮╱╱╭╮╱╱╱╱╱╱╱╭━━━╮
+┃╭━╮┃╱╱┃┃╱╱╱╱╱╱╱┃╭━━╯
+┃╰━━┳━━┫╰━┳━━┳━╮┃╰━━┳╮╭┳━━┳━┳━━┳━━┳━━╮
+╰━━╮┃╭╮┃╭╮┃╭╮┃╭╯┃╭━━┻╋╋┫╭╮┃╭┫┃━┫━━┫━━┫
+┃╰━╯┃╭╮┃╰╯┃╰╯┃┃╱┃╰━━┳╋╋┫╰╯┃┃┃┃━╋━━┣━━┃
+╰━━━┻╯╰┻━━┻━━┻╯╱╰━━━┻╯╰┫╭━┻╯╰━━┻━━┻━━╯
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱┃┃
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰╯
+      
+      """) # apas triplas, mantém as quebras de linhas da string
+
+print("Menu")
+print("1. Cadastrar restaurante")
+print("2. Listar restaurante")
+print("3. Ativar restaurante")
+print("4. Sair\n")
+
+opcao_escolhida = input("Escolha uma opção: ") #para nome composto de variável o Python usa o Snake Case (underscore case)
+print(f"Você escolheu apção {opcao_escolhida}")
+
+
+
